8f2c8d1d765cecba248ff8149419e985dcdbc0c7
Enzo Norese <enzo.norese@prodigio.tech>
2021-11-18 14:09:50 -0300

    ENV updateRepos

--- updateRepos.bat
+++ updateRepos.bat
@@ -1,39 +1,22 @@
-cd C:\Users\eNorese\Documents\TurismoReal
+cd C:\Users\$env:UserName\Documents\TurismoReal
 
 cd turismo-real-api-departamentos
 git pull
-cd ..
-
-cd turismo-real-api-login
+cd ../turismo-real-api-login
+git pull
+cd ../turismo-real-api-reservas
+git pull
+cd ../turismo-real-api-servicios
+git pull
+cd ../turismo-real-api-usuarios
+git pull
+cd ../turismo-real-api-utils
+git pull
+cd ../turismo-real-front-desktop
+git pull
+cd ../turismo-real-front-web
+git pull
+cd ../turismo-real-database
 git pull
 cd ..
-
-cd turismo-real-api-reservas
-git pull
-cd ..
-
-cd turismo-real-api-servicios
-git pull
-cd ..
-
-cd turismo-real-api-usuarios
-git pull
-cd ..
-
-cd turismo-real-api-utils
-git pull
-cd ..
-
-cd turismo-real-front-desktop
-git pull
-cd ..
-
-cd turismo-real-front-web
-git pull
-cd ..
-
-cd turismo-real-database
-git pull
-cd ..
-
 echo "Update Finished."
